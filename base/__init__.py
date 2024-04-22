33448af920182b6168978d8190d60b8434f362d8
Ramil <tlsmscience@gmail.com>
2024-04-22 14:03:58 +0500

    Текст

--- base/__init__.py
+++ base/__init__.py
@@ -2,15 +2,20 @@ import telebot
 
 bot = telebot.TeleBot('6757514713:AAFbwHmv4BZMHHtqkf9WBA2YsDNEUbsHHgc')
 
-@bot.message_handler(commands=[
-    'start',
-    'main',
-    'help'
-])
+@bot.message_handler(commands=['start'])
 def main(message):
     bot.send_message(message.chat.id, message)
 
+@bot.message_handler(commands=['main'])
+def main(message):
+    bot.send_message(message.chat.id, f'Игрок {message.from_user.first_name} '
+                                      f'{message.from_user.last_name}')
 
+@bot.message_handler()  #Этот метод должен обрабатываться снизу
+def info(message):
+
+    if message.text.lower() == 'привет':
+        bot.send_message(message.chat.id, f'{"Таможня"}')
 bot.polling(non_stop=True)
 
 
