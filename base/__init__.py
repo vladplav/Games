5eefc49039ef92139b0c630dbc58cc07f3f9a6e5
Ramil <tlsmscience@gmail.com>
2024-04-22 13:09:52 +0500

    Получение токена

--- base/__init__.py
+++ base/__init__.py
@@ -1,5 +1,28 @@
-def start():
-    return 'Start'
+import telebot
 
-if __name__ == '__main__':
-    print(start())
+bot = telebot.TeleBot('6757514713:AAFbwHmv4BZMHHtqkf9WBA2YsDNEUbsHHgc')
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# def start():
+#     return 'Start'
+#
+# if __name__ == '__main__':
+#     print(start())
