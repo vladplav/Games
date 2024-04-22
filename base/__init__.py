34c20413799772b999636ffb9034527e86039c11
Ramil <tlsmscience@gmail.com>
2024-04-22 13:45:48 +0500

    В ответ на старт, выводится сообщение привет

--- base/__init__.py
+++ base/__init__.py
@@ -2,10 +2,12 @@ import telebot
 
 bot = telebot.TeleBot('6757514713:AAFbwHmv4BZMHHtqkf9WBA2YsDNEUbsHHgc')
 
+@bot.message_handler(commands=['start'])
+def main(message):
+    bot.send_message(message.chat.id, 'test!')
 
 
-
-
+bot.polling(non_stop=True)
 
 
 
